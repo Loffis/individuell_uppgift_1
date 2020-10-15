a5b379fe6d1bdb4e8c684c8b27b48320dc75f441
loffis <loffe.knutsson@gmail.com>
2020-10-15 22:54:51 +0200

    Add translateToRobberLanguage method

--- src/main/java/se/ecutb/loffe/Solution.java
+++ src/main/java/se/ecutb/loffe/Solution.java
@@ -10,8 +10,44 @@ public class Solution
     {
         //fizzBuzz();
 
-        List<Integer> list = List.of(3, 8, 6, 5, 5, 4, 9, 1, 12, 42, 8, 6, 1, 2, 4, 6, 42);
-        System.out.println(removeDoublets(list));
+        //List<Integer> list = List.of(3, 8, 6, 5, 5, 4, 9, 1, 12, 42, 8, 6, 1, 2, 4, 6, 42);
+        //System.out.println(removeDoublets(list));
+
+        String toTranslate = "Hej på dig!";
+        System.out.println(translateToRobberLanguage(toTranslate));
+    }
+
+    private static String translateToRobberLanguage(String string) {
+        char[] charArrayToTranslate = string.toCharArray();
+        String translated = "";
+
+        for (char c : charArrayToTranslate) {
+            if (c == 'b' || c == 'B' ||
+                c == 'c' || c == 'C' ||
+                c == 'd' || c == 'D' ||
+                c == 'f' || c == 'F' ||
+                c == 'g' || c == 'G' ||
+                c == 'h' || c == 'H' ||
+                c == 'j' || c == 'J' ||
+                c == 'k' || c == 'K' ||
+                c == 'l' || c == 'L' ||
+                c == 'm' || c == 'M' ||
+                c == 'n' || c == 'N' ||
+                c == 'p' || c == 'P' ||
+                c == 'q' || c == 'Q' ||
+                c == 'r' || c == 'R' ||
+                c == 's' || c == 'S' ||
+                c == 't' || c == 'T' ||
+                c == 'v' || c == 'V' ||
+                c == 'w' || c == 'W' ||
+                c == 'x' || c == 'X' ||
+                c == 'z' || c == 'Z') {
+                translated = translated.concat(c + "o" + String.valueOf(c).toLowerCase());
+            } else {
+                translated = translated.concat(c + "");
+            }
+        }
+        return translated;
     }
 
     private static Set<Integer> removeDoublets(List<Integer> doublets) {
