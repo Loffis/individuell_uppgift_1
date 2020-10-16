03e0edb91eadb321ed4a78b5c9d11e71eb0aecdb
loffis <loffe.knutsson@gmail.com>
2020-10-16 12:15:56 +0200

    Add translateToSwedish method

--- src/main/java/se/ecutb/loffe/Solution.java
+++ src/main/java/se/ecutb/loffe/Solution.java
@@ -3,18 +3,36 @@ package se.ecutb.loffe;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Solution
 {
     public static void main( String[] args )
     {
-        //fizzBuzz();
+        fizzBuzz();
 
-        //List<Integer> list = List.of(3, 8, 6, 5, 5, 4, 9, 1, 12, 42, 8, 6, 1, 2, 4, 6, 42);
-        //System.out.println(removeDoublets(list));
+        List<Integer> list = List.of(3, 8, 6, 5, 5, 4, 9, 1, 12, 42, 8, 6, 1, 2, 4, 6, 42);
+        System.out.println("List before removing doublets: " + list);
+        System.out.println(removeDoublets(list));
 
-        String toTranslate = "Hej på dig!";
+        String toTranslate = "En jättelång mening med STORA bokstäver i också.";
         System.out.println(translateToRobberLanguage(toTranslate));
+
+        String toTranslateToSwedish = "Enon jojätottotelolånongog momenoninongog momedod SosTotORorA bobokoksostotävoveror i ocockoksoså.";
+        System.out.println(TranslateToSwedish(toTranslateToSwedish));
+    }
+
+    private static String TranslateToSwedish(String string) {
+        String translated = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ!".contains("" + string.charAt(i)))){
+                translated = translated.concat("" + string.charAt(i));
+                i += 2;
+            } else {
+                translated = translated.concat("" + string.charAt(i));
+            }
+        }
+        return translated;
     }
 
     private static String translateToRobberLanguage(String string) {
@@ -57,11 +75,11 @@ public class Solution
     private static void fizzBuzz(){
         for (int i = 1; i <= 100; i++){
             if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.println(i + " FizzBuzz");
+                    System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
-                System.out.println(i + " Fizz");
+                System.out.println("Fizz");
             } else if (i % 5 == 0) {
-                System.out.println(i + " Buzz");
+                System.out.println("Buzz");
             }
             else {
                 System.out.println(i);
