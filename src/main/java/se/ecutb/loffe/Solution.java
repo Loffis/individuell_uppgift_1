9933180d7323c0bbd4ebc10a91806ce366487f86
loffis <loffe.knutsson@gmail.com>
2020-10-20 15:00:15 +0200

    Final. Minor fixes everywhere.

--- src/main/java/se/ecutb/loffe/Solution.java
+++ src/main/java/se/ecutb/loffe/Solution.java
@@ -2,30 +2,48 @@ package se.ecutb.loffe;
 
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
 
 public class Solution
 {
     public static void main( String[] args )
     {
+
         fizzBuzz();
 
         List<Integer> list = List.of(3, 8, 6, 5, 5, 4, 9, 1, 12, 42, 8, 6, 1, 2, 4, 6, 42);
-        System.out.println("List before removing doublets: " + list);
-        System.out.println(removeDoublets(list));
+        System.out.println("\n" + removeDoublets(list) + "\n");
 
-        String toTranslate = "En jättelång mening med STORA bokstäver i också.";
-        System.out.println(translateToRobberLanguage(toTranslate));
+        String swedishString = "Hej på dig!";
+        System.out.println(translateToRobberLanguage(swedishString));
 
-        String toTranslateToSwedish = "Enon jojätottotelolånongog momenoninongog momedod SosTotORorA bobokoksostotävoveror i ocockoksoså.";
-        System.out.println(TranslateToSwedish(toTranslateToSwedish));
+        String robberLangString = "Enon jojätottotelolånongog momenoninongog momedod SosTotORorA bobokoksostotävoveror i ocockoksoså.";
+        System.out.println(TranslateToSwedish(robberLangString) + "\n");
+
+        System.out.println("whatSquare output: " + whatSquare(4));
     }
 
+    // Riskorn
+    private static int whatSquare(double input) {
+        if (input == 0 ) {
+            return 0;
+        }
+        double rice;
+        double sum = 0;
+        for (int i = 0; i < 64; i++) {
+            rice = Math.pow(2, i);
+            sum += rice;
+            if (input < (sum + 1)) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    // Rövarspråket 2
     private static String TranslateToSwedish(String string) {
         String translated = "";
         for (int i = 0; i < string.length(); i++) {
-            if (("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ!".contains("" + string.charAt(i)))){
+            if (("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ".contains("" + string.charAt(i)))){
                 translated = translated.concat("" + string.charAt(i));
                 i += 2;
             } else {
@@ -35,43 +53,27 @@ public class Solution
         return translated;
     }
 
+    // Rövarspråket
     private static String translateToRobberLanguage(String string) {
-        char[] charArrayToTranslate = string.toCharArray();
         String translated = "";
 
-        for (char c : charArrayToTranslate) {
-            if (c == 'b' || c == 'B' ||
-                c == 'c' || c == 'C' ||
-                c == 'd' || c == 'D' ||
-                c == 'f' || c == 'F' ||
-                c == 'g' || c == 'G' ||
-                c == 'h' || c == 'H' ||
-                c == 'j' || c == 'J' ||
-                c == 'k' || c == 'K' ||
-                c == 'l' || c == 'L' ||
-                c == 'm' || c == 'M' ||
-                c == 'n' || c == 'N' ||
-                c == 'p' || c == 'P' ||
-                c == 'q' || c == 'Q' ||
-                c == 'r' || c == 'R' ||
-                c == 's' || c == 'S' ||
-                c == 't' || c == 'T' ||
-                c == 'v' || c == 'V' ||
-                c == 'w' || c == 'W' ||
-                c == 'x' || c == 'X' ||
-                c == 'z' || c == 'Z') {
-                translated = translated.concat(c + "o" + String.valueOf(c).toLowerCase());
+        for (int i = 0; i < string.length(); i++) {
+            if (("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ".contains("" + string.charAt(i)))) {
+                translated = translated.concat(string.charAt(i) + "o" + (string.charAt(i) + "").toLowerCase());
             } else {
-                translated = translated.concat(c + "");
+                translated = translated.concat(string.charAt(i) + "");
+                
             }
         }
         return translated;
     }
 
-    private static Set<Integer> removeDoublets(List<Integer> doublets) {
+    // Dublett
+    private static LinkedHashSet<Integer> removeDoublets(List<Integer> doublets) {
         return new LinkedHashSet<>(doublets);
     }
 
+    // FizzBuzz
     private static void fizzBuzz(){
         for (int i = 1; i <= 100; i++){
             if (i % 3 == 0 && i % 5 == 0) {
